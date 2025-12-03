8df1c54b591ab87945097550864f1c5daaaf2699
pauparr <pautorres55xx@gmail.com>
2025-12-03 11:42:46 +0000

    Mi mapas

---
+++ app/(tabs)/pedidos.tsx
@@ -0,0 +1,73 @@
+import { Image } from 'expo-image';
+import { StyleSheet, View, } from 'react-native';
+
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import React from 'react';
+import MapView from 'react-native-maps';
+
+const [origin, setOrigin] = React.useState({
+    latitude: 37.25,
+    longitude: -12.44,
+
+      });
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/ismael.jpg')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">PEDIDOS</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Tardanza del pedido</ThemedText>
+      </ThemedView>
+      
+
+
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
+
+
+    </ParallaxScrollView>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+
+    container: {
+    height: 400,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+    map: {
+    ...StyleSheet.absoluteFillObject,
+    },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
